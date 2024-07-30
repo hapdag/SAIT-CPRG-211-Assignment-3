c3cb607d56e7d49fcda2bc8a0c37c7a4616738b4
Si He <SiYuan.He@edu.sait.ca>
2024-07-30 13:19:08 -0600

    Created SLL and Node classes

---
+++ Assignment3/Assignment3/Utility/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Node<T>
+    {
+        private Node<T> _next;
+        private T _value;
+        public Node<T> Next { get { return _next; } set { _next = value; } }
+        public T Value { get { return _value; } set { this._value = value; } }
+        public Node(T value) 
+        {
+            this._value = value;
+            _next = null;
+        }
+
+    }
+}
---
+++ Assignment3/Assignment3/Utility/SLL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class SLL<T> : ILinkedListADT
+    {
+        private Node<T> _head;
+        private int _count;
+        public SLL() 
+        {
+        }
+        public void Add(User value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFirst(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLast(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetValue(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(User value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Replace(User value, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
